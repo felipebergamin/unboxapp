30c1d3fb14b2ee9d4263dc036c95d7ee14582cff
Felipe Bergamin <felipebergamin6@gmail.com>
2020-10-05 19:43:45 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  env: {
+    es2020: true,
+    jest: true,
+  },
+  globals: {
+    __DEV__: true,
+    FormData: true,
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/prefer-default-export': 'off',
+    'consistent-return': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'react-native/no-unused-styles': 2,
+    'react-native/no-single-element-style-arrays': 2,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'class-methods-use-this': 'off',
+    'no-underscore-dangle': 'off',
+    'import/extensions': [
+      'error',
+      {
+        ts: 'never',
+        tsx: 'never',
+        json: 'always',
+      },
+    ],
+    'no-use-before-define': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  plugins: [
+    'react',
+    'prettier',
+    '@typescript-eslint',
+    'react-native',
+    'react-hooks',
+  ],
+};
